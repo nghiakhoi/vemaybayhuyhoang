58a1e2791a4881e318129ff658a242cd2cffe13f
nghiakhoi <hnghiakhoi3@gmail.com>
2018-07-31 04:09:39 +0700

    update api test

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,47 @@ import React, { Component } from 'react';
 import '../css/App.css';
 import Content from './Content';
 import MenuMobile from './MenuMobile';
+import axios from 'axios';
+
 
 class App extends Component {
   render() {
+    axios.get('http://localhost:4000/js')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  axios.get('http://localhost:4000/vj')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  axios.get('http://localhost:4000/vn')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
     return (
       <div className="App">
         <MenuMobile />
