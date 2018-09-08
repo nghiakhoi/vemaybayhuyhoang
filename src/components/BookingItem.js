562ded5742e142ab0a5153617b2c2690b5f936ae
nghiakhoi <hnghiakhoi3@gmail.com>
2018-09-09 05:08:52 +0700

    update

--- src/components/BookingItem.js
+++ src/components/BookingItem.js
@@ -6,6 +6,30 @@ import iconfly from '../images/iconfly.png';
 
 
 class BookingItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHidden: false
+        }
+    }
+
+    handleFocus() {
+        setTimeout(() => {
+            this.setState({
+                isHidden: !this.state.isHidden
+            });
+        }, 250)
+
+    }
+    handleBlur() {
+        setTimeout(() => {
+            this.setState({
+                isHidden: false
+            });
+        }, 250)
+
+    }
+
     formatnumber = (n, currency) => {
 
         return currency + n.toFixed(0).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
@@ -14,9 +38,9 @@ class BookingItem extends Component {
         var logo = this.props.airline === "Vietjet" ? vietjetlogo : this.props.airline === "Jetstar" ? jetstarlogo : vietnamairlinelogo;
 
         return (
-            <div className="tour-item">
+            <div className="tour-item" style={{ "cursor": "pointer" }}>
                 <div className="image-wrap">
-                    <div className="img" style={{ background: 'url("' + logo + '") no-repeat center center / 65% 65%' }} />
+                    <div className="img" style={{ background: 'url("' + logo + '") no-repeat center center / 65% 65%', "margin": "0 auto" }} />
 
                 </div>
                 <div className="tour-info-wrap">
@@ -38,21 +62,70 @@ class BookingItem extends Component {
                                 <div className="col-md-3" style={{ "fontSize": "20px", "textAlign": "left", "color": "#fa6d01" }}>
                                     <p>{this.props.baseprice.toFixed(1).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.").slice(0, -2)} VND</p>
                                 </div>
-                                <div className="col-md-2">
-                                    <a style={{ "fontWeight": "bold" }} href="/">Chi tiết</a>
+                                <div className="col-md-2" >
+                                    <input type="button" className="btn btn-primary" onClick={() => this.handleFocus()} onBlur={() => this.handleBlur()} style={{ "fontWeight": "bold" }} value="Chi tiết" id="select_depart_66579" name="go" />
                                 </div>
                                 <div className="col-md-2">
-                                    <input type="button" className="btn btn-primary" style={{ "background": "#49a32b" }} value="Chọn" id="select_depart_66579" name="go" />
+                                    <input type="button" className="btn btn-success" style={{ }} value="Chọn" id="select_depart_66579" name="go" />
                                 </div>
                             </div>
+                            {
+                                this.state.isHidden ?
+
+                                    <div className="col-md-12" style={{ "marginTop": "40px", "marginBottom": "-25px", "color": "black" }}>
+                                        <div className="full-width BorderTop">
+                                            <div className="DetailPriceBox">
+                                                <table className="TablePrice" style={{ width: '100%' }} cellSpacing={5} cellPadding={5} border={0}>
+                                                    <thead>
+                                                        <tr>
+                                                            <td className="ItemHeader">Hành khách</td>
+                                                            <td className="ItemHeader">Số lượng</td>
+                                                            <td className="ItemHeader">Giá vé</td>
+                                                            <td className="ItemHeader">Thuế và phí</td>
+                                                            <td className="ItemHeader">Thành tiền</td>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>Người lớn <small>(&gt; 12 tuổi)</small></td>
+                                                            <td><strong>1</strong></td>
+                                                            <td><strong className="ItemPrice">799,000 ₫</strong></td>
+                                                            <td><strong className="ItemPrice">480,900 ₫</strong></td>
+                                                            <td><strong className="ItemPrice">1,279,900 ₫</strong></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Trẻ em <small>(2-12 tuổi)</small></td>
+                                                            <td><strong>1</strong></td>
+                                                            <td><strong className="ItemPrice">799,000 ₫</strong></td>
+                                                            <td><strong className="ItemPrice">453,400 ₫</strong></td>
+                                                            <td><strong className="ItemPrice">1,252,400 ₫</strong></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Em bé <small>(&lt; 2 tuổi)</small></td>
+                                                            <td><strong>1</strong></td>
+                                                            <td><strong className="ItemPrice">0 ₫</strong></td>
+                                                            <td><strong className="ItemPrice">110,000 ₫</strong></td>
+                                                            <td><strong className="ItemPrice">110,000 ₫</strong></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <div className="TotalPriceBox">
+                                                <strong>Tổng chi phí: </strong>
+                                                <span className="GrandTotalPrice">2,642,300</span>
+                                                <span className="Currency"> VND</span>
+                                            </div>
+                                        </div>
+
+
+                                    </div> : ""
+                            }
                         </div>
                     </div>
                     <div className="tour-price-vote">
-                        <div className="price-tour theme-color">50,00$</div>
+                        <div className="price-tour theme-color"></div>
                         <div className="tour-social">
-                            <ul className="socials">
-                            dasdsa
-                            </ul>
+                            <ul className="socials"></ul>
                         </div>
                     </div>
                 </div>
